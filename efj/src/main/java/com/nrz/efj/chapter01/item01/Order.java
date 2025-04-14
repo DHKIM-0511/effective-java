85dd1c8860548e89515a97f9c2304195a413f07c
loophyun <dhkim960511@naver.com>
2025-04-14 20:41:24 +0900

    enum: enum은 인스턴스가 하나만 만들어지는것을 보장한다

--- efj/src/main/java/com/nrz/efj/chapter01/item01/Order.java
+++ efj/src/main/java/com/nrz/efj/chapter01/item01/Order.java
@@ -7,6 +7,8 @@ public class Order {
 
     private Product product;
 
+    private OrderStatus orderStatus;
+
     //여러 생성자를 만들때, 파라미터 타입이 겹치면 정적팩토리를 고려할 수 있다
     public static Order primeOrder(Product product) {
         Order order = new Order();
