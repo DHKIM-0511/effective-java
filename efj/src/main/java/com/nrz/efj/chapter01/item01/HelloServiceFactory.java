68b9c63cc5856c36f0ae8650875adeb99a1aafaf
loophyun <dhkim960511@naver.com>
2025-04-14 22:00:23 +0900

    리플렉션으로 클래스 정보 접근 및 인스턴스 생성

--- efj/src/main/java/com/nrz/efj/chapter01/item01/HelloServiceFactory.java
+++ efj/src/main/java/com/nrz/efj/chapter01/item01/HelloServiceFactory.java
@@ -2,12 +2,15 @@ package com.nrz.efj.chapter01.item01;
 
 import com.nrz.chinese_hello.ChineseHelloService;
 import com.nrz.hello.HelloService;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Optional;
 import java.util.ServiceLoader;
 
 public class HelloServiceFactory {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+        throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         //구현체에 의존 하지 않음
         ServiceLoader<HelloService> loader = ServiceLoader.load(HelloService.class);
         Optional<HelloService> first = loader.findFirst();
@@ -18,5 +21,10 @@ public class HelloServiceFactory {
         HelloService service = new ChineseHelloService();
         System.out.println(service.hello());
 
+        //리플렉션
+        Class<?> aClass = Class.forName("com.nrz.chinese_hello.ChineseHelloService");
+        Constructor<?> constructor = aClass.getConstructor();
+        HelloService helloService = (HelloService) constructor.newInstance();
+        System.out.println(helloService.hello());
     }
 }
