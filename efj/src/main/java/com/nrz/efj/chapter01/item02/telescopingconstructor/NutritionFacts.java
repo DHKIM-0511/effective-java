7fb751074eefe7b43c63d92b89a4c2ae821a29da
loophyun <dhkim960511@naver.com>
2025-04-16 21:04:49 +0900

    점층적 생성자 패턴

---
+++ efj/src/main/java/com/nrz/efj/chapter01/item02/telescopingconstructor/NutritionFacts.java
@@ -0,0 +1,39 @@
+package com.nrz.efj.chapter01.item02.telescopingconstructor;
+
+// 점층적 생성자 패턴은 확장하기 어렵다!
+public class NutritionFacts {
+    private final int servingSize;  // (mL, 1회 제공량)     필수
+    private final int servings;     // (회, 총 n회 제공량)  필수
+    private final int calories;     // (1회 제공량당)       선택
+    private final int fat;          // (g/1회 제공량)       선택
+    private final int sodium;       // (mg/1회 제공량)      선택
+    private final int carbohydrate; // (g/1회 제공량)       선택
+
+    public NutritionFacts(int servingSize, int servings){this(servingSize, servings, 0);}
+
+    public NutritionFacts(int servingSize, int servings, int calories){
+        this(servingSize, servings, calories, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings, int calories, int fat){
+        this(servingSize, servings, calories, fat, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings, int calories, int fat, int sodium){
+        this(servingSize, servings, calories, fat, sodium, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings, int calories, int fat, int sodium, int carbohydrate){
+        this.servingSize = servingSize;
+        this.servings = servings;
+        this.calories = calories;
+        this.fat = fat;
+        this.sodium = sodium;
+        this.carbohydrate = carbohydrate;
+    }
+
+    public static void main(String[] args) {
+        //(IDE의 기능이 아니라면) 어떤 파라미터가 들어가는지 알기 어렵다
+        NutritionFacts cocaCola = new NutritionFacts(10,10);
+    }
+}
