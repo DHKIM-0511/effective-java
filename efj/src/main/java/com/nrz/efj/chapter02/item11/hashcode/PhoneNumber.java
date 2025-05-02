28fdee063bd93033f5d0987fa60e002e46888b32
loophyun <dhkim960511@naver.com>
2025-05-02 15:26:55 +0900

    Thread-safety hashCode

--- efj/src/main/java/com/nrz/efj/chapter02/item11/hashcode/PhoneNumber.java
+++ efj/src/main/java/com/nrz/efj/chapter02/item11/hashcode/PhoneNumber.java
@@ -50,17 +50,23 @@ public final class PhoneNumber {
 //    }
 
     // 해시코드를 지연 초기화하는 hashCode 메서드 - 스레드 안정성까지 고려해야 한다. (71쪽)
-    private int hashCode; // 자동으로 0으로 초기화된다.
+    private volatile int hashCode; // 자동으로 0으로 초기화된다.
 
     @Override public int hashCode() {
-        int result = hashCode;
-        if(result == 0){
-            result = Short.hashCode(areaCode);
-            result = 31 * result + Short.hashCode(prefix);
-            result = 31 * result + Short.hashCode(lineNum);
-            hashCode = result;
+        if (this.hashCode != 0) {
+            return hashCode;
         }
-        return result;
+
+        synchronized (this) {
+            int result = hashCode;
+            if (result == 0) {
+                result = Short.hashCode(areaCode);
+                result = 31 * result + Short.hashCode(prefix);
+                result = 31 * result + Short.hashCode(lineNum);
+                this.hashCode = result;
+            }
+            return result;
+        }
     }
 
     public static void main(String[] args) {
