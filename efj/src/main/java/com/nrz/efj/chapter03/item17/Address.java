a2be6064b98c344edb79d11b5bbd0cc9c1710b55
loophyun <dhkim960511@naver.com>
2025-05-11 16:26:59 +0900

    불변 객체 컴포넌트가 가변인경우

---
+++ efj/src/main/java/com/nrz/efj/chapter03/item17/Address.java
@@ -0,0 +1,33 @@
+package com.nrz.efj.chapter03.item17;
+
+public class Address {
+    private String zipCode;
+
+    private String street;
+
+    private String city;
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
---
+++ efj/src/main/java/com/nrz/efj/chapter03/item17/Person.java
@@ -0,0 +1,31 @@
+package com.nrz.efj.chapter03.item17;
+
+
+public final class Person {
+    private final Address address;
+
+    public Person(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+//        return address;
+
+        Address copy = new Address();
+        copy.setStreet(address.getStreet());
+        copy.setZipCode(address.getZipCode());
+        copy.setCity(address.getCity());
+        return copy;
+    }
+
+    public static void main(String[] args) {
+        Address seoul = new Address();
+        seoul.setCity("서울");
+
+        Person person = new Person(seoul);
+        Address pusan = person.getAddress();
+        pusan.setCity("pusan");
+
+        System.out.println(person.getAddress().getCity());
+    }
+}
