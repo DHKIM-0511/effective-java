fdf9c6f7232b7ee93f207e5da6693dde50639796
loophyun <dhkim960511@naver.com>
2025-05-11 16:26:37 +0900

    불변 객체 생성

---
+++ efj/src/main/java/com/nrz/efj/chapter03/item17/PhoneNumber.java
@@ -0,0 +1,23 @@
+package com.nrz.efj.chapter03.item17;
+
+public final class PhoneNumber {
+    public final short areaCode, prefix, lineNum;
+
+    public PhoneNumber(short areaCode, short prefix, short lineNum) {
+        this.areaCode = areaCode;
+        this.prefix = prefix;
+        this.lineNum = lineNum;
+    }
+
+    public short getAreaCode() {
+        return areaCode;
+    }
+
+    public short getPrefix() {
+        return prefix;
+    }
+
+    public short getLineNum() {
+        return lineNum;
+    }
+}
