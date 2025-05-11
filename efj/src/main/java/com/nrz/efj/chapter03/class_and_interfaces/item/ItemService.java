ef97aa812d124b716df38b48840f1e3363708a08
loophyun <dhkim960511@naver.com>
2025-05-11 14:31:24 +0900

    테스트를 위해서 필드를 private에서 package-private으로 넓히는건 가능하다.

---
+++ efj/src/main/java/com/nrz/efj/chapter03/class_and_interfaces/item/ItemService.java
@@ -0,0 +1,28 @@
+package com.nrz.efj.chapter03.class_and_interfaces.item;
+
+import com.nrz.efj.chapter03.class_and_interfaces.member.MemberService;
+
+public class ItemService {
+
+    //테스트를 위해 package private 으로 변경해도 괜찮음
+    MemberService memberService;
+    boolean osSale;
+    protected int saleRate;
+    public static final String NAME = "nrz";
+
+    //이건 권장 x => 배열 내 원소가 변경될 수 있으므로
+    public static final String[] NAMES = new String[2];
+
+    public ItemService(MemberService memberService){
+        if(memberService == null){
+            throw new IllegalArgumentException("MemberService must not be null");
+        }
+        this.memberService = memberService;
+    }
+
+    //테스트를 위해 package private getter를 만들어도 괜찮다
+//    MemberService getMemberService() {
+//        return memberService;
+//    }
+}
+
---
+++ efj/src/test/java/com/nrz/efj/chapter03/class_and_interfaces/item/ItemServiceTest.java
@@ -0,0 +1,23 @@
+package com.nrz.efj.chapter03.class_and_interfaces.item;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.nrz.efj.chapter03.class_and_interfaces.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ItemServiceTest {
+
+    @Mock
+    MemberService memberService;
+
+    @Test
+    void itemServiceConstructor(){
+        ItemService service = new ItemService(memberService);
+        assertNotNull(service);
+        assertNotNull(service.memberService);
+    }
+}
