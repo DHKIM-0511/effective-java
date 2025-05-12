71c668b9012e0e579a2a085b552145a37714a8d0
loophyun <dhkim960511@naver.com>
2025-05-12 20:59:14 +0900

    JMM을 고려했을때, final을 붙여야 할당 전에 참조되지 않는다.

---
+++ efj/src/main/java/com/nrz/efj/chapter03/item17/memorymodel/Nrz.java
@@ -0,0 +1,15 @@
+package com.nrz.efj.chapter03.item17.memorymodel;
+
+public class Nrz {
+    private final int x;
+    private final int y;
+
+    public Nrz() {
+        this.x = 1;
+        this.y = 2;
+    }
+
+    public static void main(String[] args) {
+        Nrz nrz = new Nrz();
+    }
+}
