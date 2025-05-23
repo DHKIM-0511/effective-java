4dc18e206a48b5ea95c38d6de5f5ee85087d4839
loophyun <dhkim960511@naver.com>
2025-05-23 19:27:47 +0900

    interface에는 object 메서드를 정의할 수 없다

---
+++ efj/src/main/java/com/nrz/efj/chapter03/item20/objectmethod/MyInterface.java
@@ -0,0 +1,17 @@
+package com.nrz.efj.chapter03.item20.objectmethod;
+
+import java.util.Objects;
+
+public interface MyInterface {
+//    default String toString(){
+//        return "myString";
+//    }
+//
+//    default int hashCode(){
+//        return 10;
+//    }
+//
+//    default boolean equals(Object o){
+//        return true;
+//    }
+}
