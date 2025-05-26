1a2f12bd828b77c607985b71f73fc950ac6b65ac
loophyun <dhkim960511@naver.com>
2025-05-26 23:17:09 +0900

    탑 레벨 클래스는 한 파일에 하나만 작성할것

---
+++ efj/src/main/java/com/nrz/efj/chapter03/item25/Main.java
@@ -0,0 +1,8 @@
+package com.nrz.efj.chapter03.item25;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Utensil.NAME+Dessert.NAME);
+    }
+}
---
+++ efj/src/main/java/com/nrz/efj/chapter03/item25/Utensil.java
@@ -0,0 +1,9 @@
+package com.nrz.efj.chapter03.item25;
+
+class Utensil {
+    static final String NAME = "pan";
+}
+
+class Dessert {
+    static final String NAME = "cake";
+}
