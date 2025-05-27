b3382d4195108747da72ddc0cbeb986f6a60496d
loophyun <dhkim960511@naver.com>
2025-05-27 20:04:20 +0900

    제네릭 가변인자가 안전함을 명시하여 경고 지우기

---
+++ efj/src/main/java/com/nrz/efj/chapter04/item28/safevarargs/SafeVarargsExample.java
@@ -0,0 +1,24 @@
+package com.nrz.efj.chapter04.item28.safevarargs;
+
+import java.util.List;
+
+public class SafeVarargsExample {
+    static void notSafe(List<String>... stringLists){
+        Object[] array = stringLists;
+        List<Integer>tmpList = List.of(42);
+        array[0] = tmpList;
+        String s= stringLists[0].get(0); // class cast exception 런타임에 발생함
+    }
+
+    @SafeVarargs
+    static <T> void safe(T... values){
+        for(T value: values){
+            System.out.println(value);
+        }
+    }
+
+    public static void main(String[] args) {
+        SafeVarargsExample.safe("a", "b", "c");
+        SafeVarargsExample.notSafe(List.of("a", "b", "c"));
+    }
+}
