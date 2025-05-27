7ea132bf6a585afc206870ca8e17d2d9a29e5515
loophyun <dhkim960511@naver.com>
2025-05-27 21:47:27 +0900

    한정적 제네릭 사용

---
+++ efj/src/main/java/com/nrz/efj/chapter04/item29/bounded_type/Stack.java
@@ -0,0 +1,45 @@
+package com.nrz.efj.chapter04.item29.bounded_type;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.EmptyStackException;
+import java.util.List;
+
+public class Stack<E extends Number & Serializable> {
+    private Number[] elements;
+    private int size;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    @SuppressWarnings("unchecked")
+    public Stack() {
+        this.elements = (E[]) new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(E e){
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public E pop(){
+        if(size == 0) throw new EmptyStackException();
+        @SuppressWarnings("unchecked") E res = (E) elements[--size];
+        elements[size] = null;
+        return res;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    private void ensureCapacity(){
+        if(elements.length == size) elements = Arrays.copyOf(elements, 2 * size +1);
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        for(int arg: List.of(1,2,3)) stack.push(arg);
+        while (!stack.isEmpty()) {
+            System.out.println(((int) stack.pop()));
+        }
+    }
+}
